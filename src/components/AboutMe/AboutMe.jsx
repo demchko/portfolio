af5509ebdcebf94d3924c8d1b8b6aca60e5092fc
Artem-Front-End <demchkoa@gmaol.com>
2023-08-19 00:24:48 +0300

    New Photo

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -24,7 +24,7 @@ const AboutMe = ({ about }) => {
 
     return (
         <div className={styles.container} ref={about}>
-            <img className={`${styles.img} ${isActive ? styles.active : ''}`} src='/photo.jpg' />
+            <img className={`${styles.img} ${isActive ? styles.active : ''}`} src='/me.jpg' />
             <p className={`${styles.text} ${isActive ? styles.active : ''}`}> <span>I</span>'ve been into programming since I was a kid, focusing on front-end development for about 2 years now. I've also worked with teams on projects, so I get teamwork. I love learning new stuff and creating my own things. Making clean code and getting things done efficiently matters to me.
                 <br /><br /><span>Outside</span> of coding, I'm really into basketball and soccer. These sports teach me teamwork and thinking on my feet. And when I'm not coding or playing, you can catch me reading a good book. It's my way of exploring new ideas and learning.
             </p>
